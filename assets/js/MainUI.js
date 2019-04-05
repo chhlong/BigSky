892f801551336c01d5afb17adad4ecbd7716a978
chhlong <chhlong@foxmail.com>
2019-04-06 03:28:07 +0800

    添加分享功能

--- assets/js/MainUI.js
+++ assets/js/MainUI.js
@@ -18,7 +18,12 @@ cc.Class({
        playerCtrl :{
        		default : null,
        		type : cc.Node,
-       }
+       },
+
+        ShareBtn :{
+          default : null,
+          type : cc.Node,
+       },
     },
 
     onLoad: function () {
@@ -28,6 +33,7 @@ cc.Class({
     onRegisteredEvent: function () {
         this.hero01Btn.on(cc.Node.EventType.TOUCH_END, this.onHero01Click.bind(this));
         this.hero02Btn.on(cc.Node.EventType.TOUCH_END, this.onHero02Click.bind(this));
+        this.ShareBtn.on(cc.Node.EventType.TOUCH_END, this.onShareBtnClick.bind(this));
     },
 
     onHero01Click: function (event) {
@@ -38,6 +44,22 @@ cc.Class({
 
     onHero02Click: function (event) {
     	this.playerCtrl.getComponent("PlayerCtrl").changeHero("Hero02")
+    },
+
+    onShareBtnClick: function (event) {
+      cc.loader.loadRes("texture/share",function(err,data){
+          wx.shareAppMessage({
+              title: "分享测试，陈语是个大帅比！",
+              imageUrl: data.url,
+              success(res){
+                  console.log(res)
+              },
+              fail(res){
+                  console.log(res)
+              } 
+          })
+      }); 
+
     },
 
 });
